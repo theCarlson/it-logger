30c288097091d148cd92c555d769afdea663924b
theCarlson <rjcarlson24@gmail.com>
2021-11-05 15:14:18 -0500

    logs reducer, actions, and types

---
+++ src/actions/logActions.js
@@ -0,0 +1,42 @@
+import { GET_LOGS, SET_LOADING, LOGS_ERROR } from './types';
+
+// export const getLogs = () => {
+//   return async (dispatch) => {
+//     setLoading();
+
+//     const res = await fetch('/logs');
+//     const data = await res.json();
+
+//     dispatch({
+//       type: GET_LOGS,
+//       payload: data,
+//     });
+//   };
+// };
+
+// Refactored Version with error handling of above:
+export const getLogs = () => async (dispatch) => {
+  try {
+    setLoading();
+
+    const res = await fetch('/logs');
+    const data = await res.json();
+
+    dispatch({
+      type: GET_LOGS,
+      payload: data,
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data,
+    });
+  }
+};
+
+// Sets Loading to true
+export const setLoading = () => {
+  return {
+    type: SET_LOADING,
+  };
+};
